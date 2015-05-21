463c57f5810dfef588a4a51134f4707eddc51add
gothicmindfood <colettealexander@gmail.com>
2015-05-21 14:41:25 -0700

    removed unnecessary commenting

--- init.sh
+++ init.sh
@@ -8,6 +8,5 @@ export DEBIAN_FRONTEND=noninteractive
 
 test -d /etc/puppet/modules/stdlib || puppet module install puppetlabs-stdlib --version 3.2.0 
 test -d /etc/puppet/modules/mysql || puppet module install puppetlabs-mysql --version 0.6.1 
-# puppet module install example42-puppi --version 2.1.9
 test -d /etc/puppet/modules/vcsrepo || puppet module install puppetlabs-vcsrepo 
 test -d /etc/puppet/modules/python || puppet module install stankevich-python 
